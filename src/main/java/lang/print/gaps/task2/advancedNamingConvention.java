5b344c15d2ff2decf9800c6d208d015f542efeee
Nurbek Abduraximov <84277066+Nurbek21@users.noreply.github.com>
2022-07-15 16:08:03 +0500

    Update advancedNamingConvention.java

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -3,14 +3,14 @@ package lang.print.gaps.task2;
 public class advancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
-    private Integer age;
+    private int age = 18;
     private int phoneNumber;
 
     void callToFriend() {
         System.out.println("Hello");
     }
 
-    void callByNumber(int Number) {
-        //some code
+    void callByNumber(int number) {
+        System.out.println("Call this number " + number);
     }
 }
