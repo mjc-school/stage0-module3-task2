d9f536c3694764e3fa0c6e36afc59ab55381ad02
KKariess <110602659+KKariess@users.noreply.github.com>
2022-09-06 14:20:48 +0300

    initial submittion

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int variableOne = 0;
+        int variableTwo = 2000;
+        int valueOne = 1000;
+        int variableThree = 4000;
+        int valueTwo = 3000;
+        int variableFour = 5000;
+        int variableFive = 7000;
+        int valueThree = 90000;
     }
 }
--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
+    void callToFriend() {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void callByNumber(int number) {
         //some code
     }
 }
