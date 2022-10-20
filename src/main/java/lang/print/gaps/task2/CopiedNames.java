8d916bbaf9484f71d77e6f465089d6d806982111
Daniel Loaiza <38771926+Daniel-Loaiza@users.noreply.github.com>
2022-10-19 22:49:48 -0500

    Updated

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
+        variable = 2000;
         int value = 1000;
-        int variable = 4000;
+        variable = 4000;
         int value2 = 3000;
         int variables = 5000;
         variables = 7000;
-        int value = 90000;
+        value = 90000;
     }
 }
