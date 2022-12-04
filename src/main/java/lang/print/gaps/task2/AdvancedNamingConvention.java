ec54411421ad3f9b0614d70dd41251b6c63997a9
Sanjar Akhmedov <sanjakhmedov@gmail.com>
2022-12-04 17:11:06 +0500

    task 2 is done, edited some code

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,9 +1,9 @@
 package lang.print.gaps.task2;
 
-public class advancedNamingConvention {
+public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
     void callToFriend() {
