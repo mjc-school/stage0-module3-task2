dbb1438e6cffd507dbc34056e593f9c276a0ab1a
Furse <Fursevich.dasha@mail.ru>
2023-07-14 14:06:35 +0300

    ss

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,16 +1,14 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
-        //some code
+    void callToFriend() {
     }
 
-    void callByNumber(int Number) {
-        //some code
+    void callByNumber(int number) {
     }
 }
--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package school.mjc.stage0.module3.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
+        int variable1 = 2000;
         int value = 1000;
-        int variable = 4000;
+        int variable2 = 4000;
         int value2 = 3000;
         int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int variables1 = 7000;
+        int value3 = 90000;
     }
 }
